6ee35c413d325f21e2e7a130cd6271b3fa63943f
Matthias Loibl <mail@matthiasloibl.com>
2019-05-01 01:52:23 +0200

    Add step struct comment

--- cd/pipeline.go
+++ cd/pipeline.go
@@ -9,6 +9,7 @@ type Config struct {
 	Steps []Step `json:"steps"`
 }
 
+// TODO: This is probably mostly what Drone uses. Maybe we should copy that struct :)
 type Step struct {
 	Name     string   `json:"name"`
 	Image    string   `json:"image"`
