f9e2f7150785c4fc9b042fa831b357d0495ca898
Matthias Loibl <mail@matthiasloibl.com>
2019-05-03 10:06:42 +0200

    Run acutal code in kubernetes-status check plugin

--- cmd/plugins/kubernetes-status/main.go
+++ cmd/plugins/kubernetes-status/main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("checking the status...")
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 
-	i := 0
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	if err := Main(ctx, logger); err != nil {
+		level.Error(logger).Log(
+			"msg", "failed to run",
+			"err", err,
+		)
+		os.Exit(1)
+	}
+}
+
+func Main(ctx context.Context, logger log.Logger) error {
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		return err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	labels := os.Getenv("PLUGIN_LABELS")
+	fmt.Println(labels)
+
+	watch, err := client.AppsV1().Deployments("default").Watch(metav1.ListOptions{
+		LabelSelector: labels,
+		Watch:         true,
+	})
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(5 * time.Second)
+
+	var status v1.DeploymentStatus
+
 	for {
-		fmt.Println(i)
-		i++
-
-		if i < 60 {
-			time.Sleep(time.Second)
-		} else {
-			return
+		select {
+		case <-ticker.C:
+			printStatus(status)
+		case event := <-watch.ResultChan():
+			d := event.Object.(*v1.Deployment)
+			status = d.Status
+			printStatus(status)
+		case <-ctx.Done():
+			ticker.Stop()
+			watch.Stop()
 		}
 	}
+}
+
+func printStatus(status v1.DeploymentStatus) {
+	fmt.Printf("%d of %d replicas are ready", status.ReadyReplicas, status.Replicas)
 }
