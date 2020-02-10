16cccc7b63a292bd3b0247c0ac2a419edf81c215
Matthias Loibl <mail@matthiasloibl.com>
2020-02-11 00:32:26 +0100

    Use multiline strings in run-dart.sh

--- development/run-dart.sh
+++ development/run-dart.sh
@@ -9,11 +9,17 @@ trap 'kill $(jobs -p); exit 0' EXIT
 ) &
 
 (
-  ./cmd/api/api --tls.cert ./development/signalcd.dev+6.pem --tls.key ./development/signalcd.dev+6-key.pem
+  ./cmd/api/api \
+      --tls.cert ./development/signalcd.dev+6.pem \
+      --tls.key ./development/signalcd.dev+6-key.pem \
+      --ui.assets ./ui/build
 ) &
 
 (
-  cd ui && webdev serve web:6670 --tls-cert-chain=../development/signalcd.dev+6.pem --tls-cert-key=../development/signalcd.dev+6-key.pem --auto=restart
+  cd ui && webdev serve web:6670 \
+      --tls-cert-chain=../development/signalcd.dev+6.pem \
+      --tls-cert-key=../development/signalcd.dev+6-key.pem \
+      --auto=restart
 ) &
 
 for i in `jobs -p`; do wait $i; done
