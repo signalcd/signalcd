1ed37136dbcffdb90e77dc2233162e277973bf0b
Matthias Loibl <mail@matthiasloibl.com>
2020-03-29 19:17:09 +0200

    Use window.location as basePath in API calls

--- ui/lib/api.dart
+++ ui/lib/api.dart
@@ -1,10 +1,17 @@
+import 'dart:html';
+
 import 'package:ui/src/api/api.dart';
 
 class API {
   UIServiceApi ui;
 
   API() {
-    ApiClient client = ApiClient(basePath: "/");
+    String base = window.location.toString();
+    if (base.endsWith("/")) {
+      base = base.substring(0, base.length - 1);
+    }
+
+    ApiClient client = ApiClient(basePath: base);
     this.ui = UIServiceApi(client);
   }
 }
