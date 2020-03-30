fcde2a219b4a41f1c58023766a008ffcefe7c8cf
Matthias Loibl <mail@matthiasloibl.com>
2020-03-31 01:21:03 +0200

    Add agent to development run.sh and run-dart.sh

--- development/run.sh
+++ development/run.sh
@@ -15,4 +15,15 @@ trap 'kill $(jobs -p); exit 0' EXIT
       --ui.assets ./ui/build
 ) &
 
+(
+  ./cmd/agent/agent \
+      --api.url 127.0.0.1:6661 \
+      --kubeconfig ~/.kube/config \
+      --name 'local' \
+      --namespace signalcd-demo \
+      --serviceaccount=signalcd-agent \
+      --tls.cert ./development/signalcd.dev+6.pem \
+      --tls.key ./development/signalcd.dev+6-key.pem
+) &
+
 for i in `jobs -p`; do wait $i; done
