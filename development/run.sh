5d401c80f172366b3267ecb369773195285a2ab1
Matthias Loibl <mail@matthiasloibl.com>
2020-06-27 17:01:07 +0200

    Improve run.sh

--- development/run.sh
+++ development/run.sh
@@ -5,17 +5,21 @@ set -euo pipefail
 trap 'kill $(jobs -p); exit 0' EXIT
 
 (
+  echo "running api"
   ./cmd/api/api \
       --ui.assets ./ui
 ) &
 
+sleep 1
+
 (
+  echo "running agent"
   ./cmd/agent/agent \
-      --api.url 127.0.0.1:6661 \
+      --api.url http://127.0.0.1:6660 \
       --kubeconfig ~/.kube/config \
       --name 'local' \
       --namespace signalcd-demo \
       --serviceaccount=signalcd-agent
 ) &
 
-for i in `jobs -p`; do wait $i; done
+for i in $(jobs -p); do wait "$i"; done
