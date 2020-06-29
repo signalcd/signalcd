b4e6dcc84c45152c7953fdd6263960fc21a85462
Matthias Loibl <mail@matthiasloibl.com>
2020-06-29 02:46:09 +0200

    Add a button to deploy a Pipeline as new Deployment to UI

--- ui/components/app.js
+++ ui/components/app.js
@@ -1,9 +1,16 @@
 import {css, html, LitElement} from 'lit-element';
-import {ApiClient, Deployment, DeploymentApi, PipelineApi} from '../../api/client/javascript/src/index.js';
+import {
+    ApiClient,
+    Deployment,
+    DeploymentApi,
+    PipelineApi,
+    SetCurrentDeployment
+} from '../../api/client/javascript/src/index.js';
 
 class App extends LitElement {
     static get properties() {
         return {
+            client: {type: Object},
             deployments: {type: Array},
             pipelines: {type: Array},
         };
@@ -31,11 +38,26 @@ class App extends LitElement {
                 grid-template-columns: 1fr 1fr;
                 grid-column-gap: 1rem;
             }
+            ul.list {
+                list-style: none;
+                margin: 0;
+                padding: 0;
+            }
+            ul.list li {
+                border-top: 1px solid #ddd;
+                padding: 1rem;
+            }
+            ul.list li:first-child {
+                border-top: none;
+            }
         `;
     }
 
     constructor() {
         super();
+
+        this.client = new ApiClient();
+        this.client.basePath = `${window.location.protocol}//${window.location.host}/api/v1`;
 
         this.deployments = [];
         this.pipelines = [];
@@ -52,17 +74,29 @@ class App extends LitElement {
                 <div class="grid">
                     <div>
                         <h3>Deployments</h3>
-                        <ul>
+                        <ul class="list">
                             ${this.deployments.map((d) => html`
-                                <li>${d.number}</li>
+                                <li style="display: grid; grid-template-columns: 1fr 1fr auto">
+                                    <span>#${d.number}</span>
+                                    <span>Pipeline: ${d.pipeline.name}</span>
+                                    <span>${d.status === undefined ? '' : Object.keys(d.status).map((agent) => {
+                                            if (d.status[agent] !== undefined) {
+                                                return html`${d.status[agent].steps.length} steps`;
+                                            }
+                                        })
+                                    }</span>
+                                </li>
                             `)}
                         </ul>
                     </div>
                     <div>
                         <h3>Pipelines</h3>
-                        <ul>
+                        <ul class="list">
                             ${this.pipelines.map((p) => html`
-                                <li>${p.id}</li>
+                                <li style="display: grid; grid-template-columns: 1fr auto">
+                                    <span title="${p.id}">${p.name}</span>
+                                    <button @click="${() => this.deployPipeline(p.id)}">Deploy</button>
+                                </li>
                             `)}
                         </ul>
                     </div>
@@ -72,19 +106,20 @@ class App extends LitElement {
     }
 
     firstUpdated(changedProperties) {
-        let client = new ApiClient();
-        client.basePath = `${window.location.protocol}//${window.location.host}/api/v1`;
+        new DeploymentApi(this.client).listDeployments().then((deployments) => this.deployments = deployments);
+        new PipelineApi(this.client).listPipelines().then((pipelines) => this.pipelines = pipelines);
 
-        new DeploymentApi(client).listDeployments().then((deployments) => this.deployments = deployments);
-        new PipelineApi(client).listPipelines().then((pipelines) => this.pipelines = pipelines);
-
-        let deploymentsEvents = new EventSource(`${client.basePath}/deployments/events`);
+        let deploymentsEvents = new EventSource(`${this.client.basePath}/deployments/events`);
         deploymentsEvents.onmessage = (event) => this.updateDeployments(event);
     }
 
     updateDeployments(event) {
         let deployment = Deployment.constructFromObject(JSON.parse(event.data));
         this.deployments = [deployment, ...this.deployments];
+    }
+
+    deployPipeline(id) {
+        new DeploymentApi(this.client).setCurrentDeployment(new SetCurrentDeployment(id));
     }
 }
 
